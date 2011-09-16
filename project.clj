c184e940afe10286c4b813e1dc7e9a71a1b0ec1d
Nathan Marz <nathan@twitter.com>
2011-09-15 22:13:59 -0700

    use storm 0.5.0

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :dependencies []
-  :dev-dependencies [[storm "0.0.3-SNAPSHOT"]]
+  :dev-dependencies [[storm "0.5.0"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )
