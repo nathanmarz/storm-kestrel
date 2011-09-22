8b804e8f7929e38616fab8cc10835ec3182dd8b1
Nathan Marz <nathan@twitter.com>
2011-09-22 16:41:20 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-kestrel "0.5.0"
+(defproject storm/storm-kestrel "0.5.1-SNAPSHOT"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :dependencies []
