24486aa7294ec49aae4660fac432e5d5c8cb2af3
Nathan Marz <nathan@twitter.com>
2011-10-25 01:11:31 -0700

    removed unnecessary KestrelAdder class

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject storm/storm-kestrel "0.5.1-SNAPSHOT"
+(defproject storm/storm-kestrel "0.5.4"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :dependencies []
-  :dev-dependencies [[storm "0.5.0"]]
+  :dev-dependencies [[storm "0.5.4"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )
