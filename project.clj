60b36cd1b4ed59c9b36010b87a22a43c5b144d23
Jason Jackson <jason@cvk.ca>
2011-11-01 14:24:57 -0700

    added kestrel v3 spout

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject storm/storm-kestrel "0.5.1-SNAPSHOT"
+(defproject storm/storm-kestrel "0.6.0-SNAPSHOT"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :dependencies []
+  :dependencies [[kestrel-thrift-java-bindings "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[storm "0.5.0"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )
