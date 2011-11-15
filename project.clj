e21639e54b18911ce23d86563e7deed8a135b138
Jason Jackson <jason@cvk.ca>
2011-11-14 18:40:25 -0800

    kestrel-thrift-java-bindings wasnt on clojars, not it is

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject storm/storm-kestrel "0.6.0-SNAPSHOT"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :dependencies [[kestrel-thrift-java-bindings "1.0.0-SNAPSHOT"]]
+  :dependencies [[kestrel-thrift-java-bindings "0.5.0"]]
   :dev-dependencies [[storm "0.5.4"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )
