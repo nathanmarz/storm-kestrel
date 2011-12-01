a244d89d17776854fcc0c773ae935aba20e178de
Nathan Marz <nathan@twitter.com>
2011-11-30 17:41:24 -0800

    remove thrift spout for now

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject storm/storm-kestrel "0.6.0"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :dependencies [[kestrel-thrift-java-bindings "0.5.0"]]
+  :dependencies []
   :dev-dependencies [[storm "0.6.0"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )
