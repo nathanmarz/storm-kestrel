d380930b9cdea5d7f6896abc3d6c795cc2929dc6
Nathan Marz <nathan@twitter.com>
2012-04-02 12:59:12 -0700

    0.7.1 compatible

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject storm/storm-kestrel "0.7.0"
+(defproject storm/storm-kestrel "0.7.1"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :dependencies [[kestrel-thrift-java-bindings "2.2.0"]]
-  :dev-dependencies [[storm "0.7.0"]]
+  :dev-dependencies [[storm "0.7.1"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )
