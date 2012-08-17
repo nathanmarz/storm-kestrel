a8c274b194a873d756cc646034456a60ad780146
Robbie Strickland <rstrickland@weather.com>
2012-08-17 17:20:24 -0400

    Removed unneccessary import of Values

--- src/jvm/backtype/storm/spout/KestrelThriftSpout.java
+++ src/jvm/backtype/storm/spout/KestrelThriftSpout.java
@@ -11,7 +11,6 @@ import java.util.Iterator;
 import java.util.Queue;
 import java.util.LinkedList;
 import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 import java.util.ArrayList;
 import java.util.Arrays;
