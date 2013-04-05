0fd61b6cc34a6a4326bf48badb3b0e2e5c4a78c0
Gary Malouf <gary@garymalouf.com>
2013-04-04 23:44:58 -0400

    Make project work for lein 2.

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject storm/storm-kestrel "0.7.2-snap3"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
-  :javac-options {:debug "true" :fork "true"}
+(defproject storm/storm-kestrel "0.7.2-snap3-mx-1-SNAPSHOT"
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
+  :javac-options {:debug "true"}
   :dependencies []
-  :dev-dependencies [[storm "0.7.0"]
-                     [org.clojure/clojure "1.2.0"]
-                     [org.clojure/clojure-contrib "1.2.0"]]
-  :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"])
+  :profiles {:dev
+              {:dependencies [[storm "0.8.0"]
+                              [org.clojure/clojure "1.2.0"]
+                     [org.clojure/clojure-contrib "1.2.0"]],
+               :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]}}
+  :min-lein-version "2.0.0")
